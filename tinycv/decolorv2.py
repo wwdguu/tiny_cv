956c49c64e5a18ee16fe58047afae8bf4b53a0c8
fengyang <fengyang@fengyang.cloud.com>
2018-11-20 21:40:18 +0800

    update decolor

--- tinycv/decolorv2.py
+++ tinycv/decolorv2.py
@@ -1,22 +1,23 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.linalg as linalg
 
 def compute_delta_alpha_l(src_img):
     src_img=src_img.astype(np.float32)/255
     Lab=cv2.cvtColor(src_img,cv2.COLOR_BGR2LAB)
     L,a,b=cv2.split(Lab)
+    print(np.min(L),np.max(L))
+    print(np.min(a),np.max(a))
+    print(np.min(b),np.max(b))
     h,w=src_img.shape[:2]
-    delta=np.zeros((2*h*w),)
+    delta=np.zeros((2*h*w-w-h),)
     alpha=0.5*np.ones_like(delta)
     index=0
-    l=np.zeros((2*h*w,9))
+    l=np.zeros((2*h*w-w-h,9))
     b, g, r = cv2.split(src_img)
     M=np.concatenate((r[:,:,np.newaxis],g[:,:,np.newaxis],b[:,:,np.newaxis],
                       (r*g)[:,:,np.newaxis],(r*b)[:,:,np.newaxis],(g*b)[:,:,np.newaxis],
                       (r*r)[:,:,np.newaxis],(g*g)[:,:,np.newaxis],(b*b)[:,:,np.newaxis]),axis=2)
-
     for i in range(h):
         for j in range(w):
            if (i+1)<h:
@@ -25,12 +26,6 @@ def compute_delta_alpha_l(src_img):
                    alpha[index]=1.
                l[index,:]=M[i,j]-M[i+1,j]
                index+=1
-           #else:
-           #     delta[index]=0.
-           #     l[index,:]=0.
-           #     index+=1
-    for i in range(h):
-        for j in range(w):
            if (j+1)<w:
                delta[index] = np.sqrt(
                    (L[i, j] - L[i, j+1]) ** 2 + (a[i, j] - a[i, j+1]) ** 2 + (b[i, j] - b[i, j+1]) ** 2)
@@ -38,41 +33,30 @@ def compute_delta_alpha_l(src_img):
                    alpha[index]=1.
                l[index, :] = M[i, j] - M[i, j+1]
                index += 1
-           #else:
-           #    delta[index]=0.
-           #    l[index,:]=0.
-           #    index+=1
-    print(index)
-    delta=delta[:index]/100
-    alpha=alpha[:index]
-    l=l[:index]
-    return  delta,alpha,l,M
+    return  delta/100,alpha,l,M
 
 def compute_G(delta_g,delta,sigma):
-    return np.exp(-((delta_g-delta)**2)/(sigma))
+    return np.exp(-(delta_g-delta)**2/(2*sigma**2))
 
-def update_beta(alpha,delta,omega,l):
+def update_beta(alpha,delta,omega,l,eps=1e-20):
     delta_g=l.dot(omega)
     sigma = float(0.69 * np.std(delta_g))
-    sigma=2e-3
+    sigma=0.02
     p_G=compute_G(delta_g,delta,sigma)
     n_G=compute_G(delta_g,-delta,sigma)
-    p_G[p_G<1e-5]=0.
-    n_G[n_G<1e-5]=0.
-    beta=alpha*p_G/(alpha*p_G+(1-alpha)*n_G+1e-20)
-    print(beta[:10])
+    beta=alpha*p_G/(alpha*p_G+(np.ones_like(alpha)-alpha)*n_G)
     return beta
 
 def update_omega(delta,beta,l):
     A=l.T.dot(l)
     b=l.T.dot((2*beta-np.ones_like(beta))*(delta))
-    omega=np.linalg.solve(A,b,)
-    return omega
+    w=np.linalg.solve(A,b)
+    return w
 
 
 
 if __name__=='__main__':
-    src_img=cv2.imread('decolor_img/6.png',1)
+    src_img=cv2.imread('decolor_img/6.png')
     h,w=src_img.shape[:2]
     delta,alpha,l,M=compute_delta_alpha_l(src_img)
     omega=np.zeros((9,))
@@ -94,8 +78,8 @@ if __name__=='__main__':
     hist=np.bincount(gray.reshape(-1,))
     plt.plot(range(len(hist)),hist)
     plt.show()
-    plt.imshow(gray,cmap='gray')
-    plt.show()
+    cv2.imshow('g',gray)
+    cv2.waitKey(0)
     print(gray.shape)
 
     gray2,_=cv2.decolor(src_img)
@@ -104,10 +88,6 @@ if __name__=='__main__':
     plt.show()
     min_c, max_c = np.min(gray2), np.max(gray2)
     print(min_c, max_c)
-    plt.imshow(gray2,cmap='gray')
-    plt.show()
+    cv2.imshow('g', gray2)
+    cv2.waitKey(0)
     print(gray2.shape)
-
-
-
-
